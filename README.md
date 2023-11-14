b8b21feb814db23878b9241723a004b8337451ca
Kevin Fula <kevin.fula@edu.sait.ca>
2023-11-13 22:26:01 -0700

    added tailwind to js files

---
+++ README.md
@@ -0,0 +1,12 @@
+# create-svelte
+
+Everything you need to build a Svelte library, powered by [`create-svelte`](https://github.com/sveltejs/kit/tree/master/packages/create-svelte).
+
+Read more about creating a library [in the docs](https://kit.svelte.dev/docs/packaging).
+
+# Help for input handling:
+https://www.youtube.com/watch?v=n8Kk7uvsx9A
+
+
+
+> To deploy your app, you may need to install an [adapter](https://kit.svelte.dev/docs/adapters) for your target environment.
---
+++ src/routes/+page.svelte
@@ -0,0 +1,31 @@
+<script>
+let name = 'Kevin';
+let colour = 'red';
+
+const handleClick = () => {
+  colour = 'blue';
+};
+
+const functionInput = (e) => {
+ colour = e.target.value
+
+}
+</script>
+
+<main class= "text-center p-4, max-width-96px, margin-40px-auto">
+
+<h1 class="text-3xl font-bold underline">Hello {name}</h1>
+<div class="pointer-events-none focus:pointer-events-auto"></div>
+<p>{colour}: colours</p>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<button on:click={handleClick}>Update the color</button>
+<input type="text" on:input={functionInput}>
+  
+</main>
+
+<style lang="postcss">
+  :global(html) {
+    background-color: theme(colors.gray.100);
+  }
+  </style>
+
